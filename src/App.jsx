56c53691116b9841ea3636a884f77687668ac60d
yamanaka-kazuki <kazuki.yamanaka0013@gmail.com>
2023-11-11 15:55:13 +0900

    feat: シルエットクイズ機能

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider, Heading } from '@chakra-ui/react';
+import { PokeCard } from './components/organisms/PokeCard';
 
-function App() {
+import { theme } from './Theme';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <div className="App">
+        <Heading mt="24">ポケモンシルエットクイズ</Heading>
+        <PokeCard />
+      </div>
+    </ChakraProvider>
   );
 }
-
-export default App;
---
+++ src/components/organisms/PokeCard.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Card,
+  CardBody,
+  Stack,
+  Heading,
+  Image,
+  Box,
+  AbsoluteCenter,
+  Input,
+  Button,
+  useToast,
+} from '@chakra-ui/react';
+import axios from 'axios';
+
+export const PokeCard = () => {
+  const [data, setData] = useState([]);
+  const [imgUrl, setImgUrl] = useState('');
+
+  useEffect(() => {
+    const randomNum = Math.floor(Math.random() * 150 + 1);
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${randomNum}`
+        );
+        console.log(response.data);
+        setImgUrl(
+          response.data.sprites.other['official-artwork'].front_default
+        );
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching Data', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const [value, setValue] = useState('');
+  const [isCorrect, setIsCorrect] = useState(false);
+  const toast = useToast();
+
+  const handleInputChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const judgeAnswer = () => {
+    const isCorrect = value === data.name;
+    setIsCorrect(isCorrect);
+    trueFalseToast(isCorrect);
+  };
+
+  const trueFalseToast = (isCorrect) => {
+    toast({
+      title: isCorrect ? '正解！！' : '不正解。。。',
+      status: isCorrect ? 'success' : 'error',
+      duration: 1000,
+      position: 'top',
+    });
+
+    if (isCorrect) {
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    }
+  };
+
+  return (
+    <>
+      <Box position="relative" h="600px">
+        <AbsoluteCenter>
+          <Card w="480px" maxW="lg" boxShadow="lg">
+            <CardBody>
+              <Stack align="center">
+                <Image
+                  src={imgUrl}
+                  borderRadius="lg"
+                  w="200"
+                  h="200"
+                  style={
+                    isCorrect
+                      ? { filter: 'brightness(100%)' }
+                      : { filter: 'brightness(0%)' }
+                  }
+                ></Image>
+                <Heading size="md">このポケモンは？</Heading>
+                <Input
+                  placeholder="名前を入力"
+                  borderRadius="lg"
+                  mt="2"
+                  onChange={handleInputChange}
+                />
+                <Button colorScheme="teal" size="md" onClick={judgeAnswer}>
+                  正解を表示
+                </Button>
+              </Stack>
+            </CardBody>
+          </Card>
+        </AbsoluteCenter>
+      </Box>
+    </>
+  );
+};
